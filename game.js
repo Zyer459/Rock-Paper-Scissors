2f6240e10ce9ff9f27dfe314c665e7830ecc7a94
Zyer459 <asiful.sumit@gmail.com>
2022-12-18 19:43:33 +0600

    added game, gameround, score functions & 1 loop

--- game.js
+++ game.js
@@ -1,18 +1,86 @@
+function UserChoice () {
+    User = prompt ("rock, paper or scissor?");
+    User = User.toLowerCase ();
+
+    if (User == "rock") {
+        User = "rock";
+    }
+    else if (User == "paper") {
+        User = "paper";
+    }
+    else if (User == "scissor") {
+        User = "scissor";
+    }
+    else {
+        User = "Error"
+    }
+    return User
+}
+
 function ComputerChoice (min=1, max=3) {
     let PC = "";
     switch (Math.floor (Math.random() * (max - min + 1)) + min) {
         case 1:
-            PC = "Rock";
-            break;
+            PC = "rock";
+            return PC
         case 2:
-            PC ="Paper";
-            break;
+            PC ="paper";
+            return PC
         case 3:
-            PC ="Scissors"
-            break;
-        default:
-            PC = "Error";
+            PC ="scissors"
+            return PC
     }
-    return console.log (PC);
 }
-console.log (ComputerChoice());
+
+function Game (User, PC, score,  result) {
+    User = UserChoice();
+    PC = ComputerChoice();
+
+    function GameRound (result) {
+        for (let i = 0; i < 5; i++) {
+            function Score (Uscore, Comscore, result) {
+                Uscore = 0, Comscore = 0;
+
+                if (User == "rock" && PC == "paper") {
+                    Uscore += 0;
+                    Comscore += 1;
+                }
+                else if (User = "rock" && PC == "scissor") {
+                    Uscore += 1;
+                    Comscore += 0;
+                }
+                else if (User = "paper" && PC == "scissor") {
+                    Uscore += 0;
+                    Comscore += 1;
+                }
+                else if (User = "paper" && PC == "rock") {
+                    Uscore += 1;
+                    Comscore += 0;
+                }
+                else if (User = "scissor" && PC == "rock") {
+                    Uscore += 0;
+                    Comscore += 1;
+                }
+                else if (User = "scissor" && PC == "paper") {
+                    Uscore += 1;
+                    Comscore += 0;
+                }
+                else {
+                    Uscore += 0;
+                    Comscore += 0;
+                }
+
+                if (Uscore > Comscore) {
+                    result = "You win :D";
+                }
+                else if (Uscore < Comscore) {
+                    result = "You lose :("
+                }
+                else {
+                    result = "Draw :o"
+                }
+            }
+        }
+    }
+}
+console.log (Game());
