5a641c36cbd7f2bfc641394411716ed53ccd5cd9
Zyer459 <asiful.sumit@gmail.com>
2022-12-18 14:50:04 +0600

    blank html & Computer Choice Function

---
+++ game.js
@@ -0,0 +1,19 @@
+function ComputerChoice (min=1, max=3) {
+    let PC = "";
+
+    switch (Math.floor (Math.random() * (max - min + 1)) + min) {
+        case 1:
+            PC = "Rock";
+            break;
+        case 2:
+            PC ="Paper";
+            break;
+        case 3:
+            PC ="Scissors"
+            break;
+        default:
+            PC = "Error";
+    }
+    return console.log (PC);
+}
+console.log (ComputerChoice());
